27581c541ff169f422ac331b99cbee29e2cce14e
softwaretestlab <softwaretestlab@hotmail.com>
2024-09-20 18:23:07 +1000

    FirstClass

---
+++ src/main/java/JavaBasicsLearning/AT02_LearnAboutMethod_JUnit.java
@@ -0,0 +1,16 @@
+package JavaBasicsLearning;
+
+import org.junit.Test;
+
+public class AT02_LearnAboutMethod_JUnit {
+
+    @Test
+    public void test01(){
+            System.out.println("This is method-1");
+        }
+    @Test
+    public void test02(){
+        System.out.println("This is method-2");
+    }
+
+}
