0445a4f6151d6a063fc4e3779f9b5e1f34b43eec
softwaretestlab <softwaretestlab@hotmail.com>
2024-09-30 17:24:09 +1000

    selenium

---
+++ src/test/java/LoopsNConditionalStatments/AT02_WhileLoop.java
@@ -0,0 +1,15 @@
+package LoopsNConditionalStatments;
+
+import org.junit.Test;
+
+public class AT02_WhileLoop {
+    @Test
+    public void WhileLoopLearn() {
+        int count =55;
+        while(count<60){
+            System.out.println("count value is : "+count);
+            count++;
+        }
+    }
+
+}
