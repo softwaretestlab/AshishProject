aaaa6fff1837ebf145ecae046adce30bfebed4fb
softwaretestlab <softwaretestlab@hotmail.com>
2024-11-06 17:19:44 +1100

    double

---
+++ src/test/java/SeleniumLearning/AT17_AssertImplementation.java
@@ -0,0 +1,13 @@
+package SeleniumLearning;
+//import common.AssertResponse;
+import org.junit.Test;
+
+//public class AT17_AssertImplementation {
+//
+//    @Test
+//    public void AssertStringRes(){
+//        String expected = "SoftwareTestLab";
+//        String actual="SoftwareTestLab";
+//        AssertRe
+//    }
+//}
